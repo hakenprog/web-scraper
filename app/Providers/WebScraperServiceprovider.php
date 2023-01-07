47a3866b22281057391c0b9c655065756df70df7
David Barrera <hakenprog@gmail.com>
2023-01-07 16:58:28 -0500

    feat: create WebScraperServiceprovider

---
+++ app/Providers/WebScraperServiceprovider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebScraperServiceprovider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
