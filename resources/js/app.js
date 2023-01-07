44c3d5a7975aed440ee64dedf00f908256386584
David Barrera <hakenprog@gmail.com>
2023-01-07 14:06:56 -0500

    feat: install and configure inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/inertia-react';
+
+createInertiaApp({
+  resolve: name => import(`./Pages/${name}`),
+  setup({ el, App, props }) {
+    const root = ReactDOM.createRoot(el);
+    root.render(<App {...props} />);
+  },
+});
