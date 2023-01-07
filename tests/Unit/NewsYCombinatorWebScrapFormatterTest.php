f9873c929de0d1834a55fd11eaf59924482ab6dd
David Barrera <hakenprog@gmail.com>
2023-01-07 18:34:52 -0500

    feat: test array formatter items format

--- tests/Unit/NewsYCombinatorWebScrapFormatterTest.php
+++ tests/Unit/NewsYCombinatorWebScrapFormatterTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Unit;
 
-use App\Interfaces\WebScraperFormatter;
 use App\Services\NewsYCombinatorScraperFormatter;
 use PHPUnit\Framework\TestCase;
 
@@ -39,10 +38,31 @@ class NewsYCombinatorWebScrapFormatter extends TestCase
     {
         $formatter = new NewsYCombinatorScraperFormatter();
         $formattedItems = $formatter->formatItems($data[0])[0];
-        $this->assertArrayHasKey("rank", $formattedItems);
-        $this->assertArrayHasKey("comments", $formattedItems);
-        $this->assertArrayHasKey("points", $formattedItems);
-        $this->assertArrayHasKey("title", $formattedItems);
+        $this->assertArrayHasKey('rank', $formattedItems);
+        $this->assertArrayHasKey('comments', $formattedItems);
+        $this->assertArrayHasKey('points', $formattedItems);
+        $this->assertArrayHasKey('title', $formattedItems);
+    }
+
+      /**
+     * A unit test for the NewsYCombinator Webscrapper formatter.
+     *
+     * @dataProvider Tests\DataProviders\NewsYCombinatorDataProvider::standard_data_provider
+     * @dataProvider Tests\DataProviders\NewsYCombinatorDataProvider::missing_points_invalid_comments_data_provider
+     * @dataProvider Tests\DataProviders\NewsYCombinatorDataProvider::invalid_comments_data_provider
+     * @dataProvider Tests\DataProviders\NewsYCombinatorDataProvider::missing_points_data_provider
+     * @dataProvider Tests\DataProviders\NewsYCombinatorDataProvider::missing_comments_data_provider
+     * @depends test_new_y_combinator_formatter_has_all_keys
+     * @return void
+     */
+    public function test_new_y_combinator_formatter_has_correct_format(array $data)
+    {
+        $formatter = new NewsYCombinatorScraperFormatter();
+        $formattedItems = $formatter->formatItems($data[0])[0];
+        $this->assertIsInt($formattedItems['rank']);
+        $this->assertIsInt($formattedItems['comments']);
+        $this->assertIsInt($formattedItems['points']);
+        $this->assertIsString($formattedItems['title']);
     }
 
 
