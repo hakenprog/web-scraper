f7f480d2748beb2f0348be52bc2ee99efd62b3a4
David Barrera <hakenprog@gmail.com>
2023-01-08 15:05:58 -0500

    fix: ignore laravel js files

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 /**jest.config.js**/
 module.exports = {
-    testRegex: 'resources/js/test/.*.test.js$',
-    collectCoverage: true,
-    collectCoverageFrom: ['./resources/js/**/*.{js,jsx}'],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'jsdom',
-    "automock": false,
-    "setupFiles": [
-      "./setupJest.js"
-    ]
-  }
+  testRegex: 'resources/js/test/.*.test.js$',
+  collectCoverage: true,
+  collectCoverageFrom: ['./resources/js/**/*.{js,jsx}'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  "automock": false,
+  "setupFiles": [
+    "./setupJest.js"
+  ],
+  coveragePathIgnorePatterns: ["app.js", 'bootstrap.js']
+}
