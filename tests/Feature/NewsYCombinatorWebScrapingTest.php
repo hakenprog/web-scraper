68844a84a6f209b70389c262193a25fbdda5d9f5
David Barrera <hakenprog@gmail.com>
2023-01-07 21:48:59 -0500

    feat: create api feature test

--- tests/Feature/NewsYCombinatorWebScrapingTest.php
+++ tests/Feature/NewsYCombinatorWebScrapingTest.php
@@ -8,15 +8,35 @@ use Tests\TestCase;
 
 class NewsYCombinatorWebScraping extends TestCase
 {
+    private $apiResponse;
+
+
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->apiResponse = $this->json('GET', '/api/v1/newsycbombinator');
+    }
+
     /**
-     * A basic feature test jjj.
+     * Ensure the web scraper api works.
      *
      * @return void
      */
-    public function test_example()
+    public function test_api_works()
     {
-        $response = $this->get('/');
+        $this->apiResponse->assertStatus(200);
+    }
 
-        $response->assertStatus(200);
+    /**
+     * Ensure the web scraper api works and returns a valid json.
+     *
+     * @return void
+     * @depends test_api_works
+     */
+    public function test_api_returns_correct_format()
+    {
+        $content = json_decode($this->apiResponse->getContent(), true);
+        $this->assertIsArray($content);
     }
 }
